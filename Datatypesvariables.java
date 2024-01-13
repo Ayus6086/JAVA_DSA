0d440123bf31a17d232648c89ee59e7a639fb8c2
AYUSH <mehtaayush6086@gmail.com>
2024-01-13 22:55:16 +0530

    datatypesandvariables

--- Datatypesvariables.java
+++ Datatypesvariables.java
@@ -1,6 +1,14 @@
 public class Datatypesvariables {
     public static void main(String[] args){
         int num = 12;
+        char a = 'a';
+        float b = 2.3f;
+        double d = 1.99;
+        String s = "HELLO JAVA";
         System.out.println(num);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(d);
     }
 }
