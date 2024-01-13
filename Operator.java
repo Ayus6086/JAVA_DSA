18437d9f2bc833dd76c73bcfd846c3b746cf6aa9
AYUSH <mehtaayush6086@gmail.com>
2024-01-13 22:49:54 +0530

    operators

---
+++ Operator.java
@@ -0,0 +1,13 @@
+public class Operator {
+    public static void main(String[] args){
+        int a = 3;
+        int b = 4;
+        int g = 5;
+        int sum = a+b+g;
+        System.out.println(sum);
+        int c = 5;
+        int d = 6;
+        System.out.println(c*d);
+
+    }
+}
--- operators.java
+++
@@ -1,7 +0,0 @@
-public class Operators  {
-    public static void main(String[] args){
-        int a = 3;
-        int b = 4;
-        System.out.println(a+b);
-    }
-}
