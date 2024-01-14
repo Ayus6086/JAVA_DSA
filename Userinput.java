26fa57ddf2d5c3059b027b15ec737333a76baee4
AYUSH <mehtaayush6086@gmail.com>
2024-01-14 20:39:18 +0530

    User

---
+++ Userinput.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Userinput {
+    public static void main(String[] args){
+        System.out.println("Taking input from user");
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a : ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter b : ");
+        // int b = sc.nextInt();
+        // int c = a+b;
+        // System.out.print("Sum equals to : ");
+        // System.out.println(c);
+        // System.out.println("Enter d : ");
+        // float d = sc.nextFloat();
+        // System.out.println("Enter e :");
+        // float e = sc.nextFloat();
+        // System.out.print("Multiplication of e and f :");
+        // float f = e*d;
+        // System.out.println(String.format("%.2f",f));
+        System.out.println("Enter your name : ");
+        String Q = sc.nextLine();
+        System.out.println(Q);
+
+
+
+
+    }
+}
