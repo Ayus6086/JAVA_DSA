cce94a9961c26cc4b8ccebadf1dfbd11cb08925c
AYUSH <mehtaayush6086@gmail.com>
2024-01-24 21:00:26 +0530

    JAVA

---
+++ Patterns.java
@@ -0,0 +1,31 @@
+public class Patterns{
+    public static void main(String args[]) {
+    //     for (int i = 0; i < 4 ; i++){
+    //         for (int j = 0; j <4 ; j++){
+    //             System.out.print("* ");
+    //     }
+    //     System.out.println("\n");
+    //     }
+
+    //     for (int i = 0; i < 4 ; i++){
+    //         for (int j = 0; j <= i ; j++){
+    //             System.out.print("* ");
+    //     }
+    //     System.out.println("\n");
+    // }
+
+    // for (int i = 0; i < 4 ; i++){
+    //         for (int j = 0; j <=i ; j++){
+    //             System.out.print(j + " ");
+    //     }
+    //     System.out.println("\n");
+    //     }
+
+        for (int i = 5; i > 0 ; i--){
+            for (int j = i; j >0 ; j--){
+                System.out.print("@ ");
+        }
+        System.out.println("\n");
+        }
+}
+}
