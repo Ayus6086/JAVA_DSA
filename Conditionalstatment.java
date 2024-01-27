4f8819caac53e1eaee09c024029330fda7033862
AYUSH <mehtaayush6086@gmail.com>
2024-01-27 22:46:39 +0530

    JAVA

--- Conditionalstatment.java
+++ Conditionalstatment.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Conditionalstatment {
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in)) {
+        Scanner in = new Scanner(System.in) ;
             System.out.println("Enter your age : ");
             int age = in.nextInt();
             if (age>=18){
@@ -10,6 +10,6 @@ public class Conditionalstatment {
             else{
                 System.out.println("You are not eligible to vote");
             }
-        }
+        
     }
 }
---
+++ LogicalOpreators.java
@@ -0,0 +1,11 @@
+public class LogicalOpreators{
+    public static void main(String[] args) {
+        int  i = 48;
+        if (i > 18 && i < 60){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not eligible");
+        }
+    }
+}
