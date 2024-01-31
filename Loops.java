06b5d74a0fbf4931d3da93f3276c4ed6925840bb
AYUSH <mehtaayush6086@gmail.com>
2024-01-31 19:59:39 +0530

    JAVA

--- Loops.java
+++ Loops.java
@@ -1,27 +1,28 @@
 public class Loops{
     public static void main(String[] args) {
-        // System.out.println("----While Loop----");//First check conditon then exceute
-        // int i = 100;
-        // while (i <= 200){
-        //     System.out.println(i);
-        //     i++;
-        // }
-        // System.out.println("----End of while Loop----");
+        System.out.println("----While Loop----");//First check conditon then exceute
+        int i = 100;
+        while (i <= 110){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("----End of while Loop----");
 
-        // System.out.println("----do while Loop----");//First exceute then check condition
-        // int a = 1;
-        // do {
-        //     System.out.println(a);
-        //     a++;
-        // } while (a <= 10);
-        // System.out.println("----End of do while Loop----");
+        System.out.println("----do while Loop----");//First exceute then check condition
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a <= 10);
+        System.out.println("----End of do while Loop----");
 
         System.out.println("----For Loop----");
         int n = 10;
         System.out.println("First n odd numbers. n = " + n);
-        for (int i = 0; i < n; i++){
-            System.out.println(2*i+1);
+        for (int k = 0; k < n; k++){
+            System.out.println(2*k+1);
         }
+        System.out.println("----End of for Loop----");
 
     }
 }
