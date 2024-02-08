4681d39d91f9f4e7baa5b12c2c913815a9a0c718
AYUSH <mehtaayush6086@gmail.com>
2024-02-08 20:58:19 +0530

    JAVA

---
+++ Constructor.java
@@ -0,0 +1,17 @@
+class Person{
+    public String name;
+    public int age;
+    public Person(){
+        System.out.println("HelloWrld");
+    }
+}
+public class Constructor {
+    public static void main(String[] args) {
+        Person  p1 = new Person();
+        Person p2 = new Person();
+        p1.age = 15;
+        p1.name = "Mahesh";
+        System.out.println(p1.age);
+        System.out.println(p1.name);
+    }
+}
