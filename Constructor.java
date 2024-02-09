adcfb66754cf6e67c318a3126f268b2427c1621c
AYUSH <mehtaayush6086@gmail.com>
2024-02-09 21:00:58 +0530

    JAVA

--- Constructor.java
+++ Constructor.java
@@ -1,18 +1,40 @@
+/* A constructor is a block of code similar to a class
+It is called when an instance of a class is created.
+
+*/
 class Person{
     public String name;
     public int age;
+    public int mob;
+    //default constructor
     public Person(){
         System.out.println("Hello World");
     }
+    public Person(String name, int age){
+        System.out.println("Para Constructor");
+        this.name = name;
+        this.age = age;
+    }
+    public Person(String name, int age, int mob){
+        System.out.println("Para Constructor");
+        this.name = name;
+        this.age = age;
+        this.mob = mob;
+}
 }
 public class Constructor {
     public static void main(String[] args) {
-        Person p1 = new Person();
+        Person p1 = new Person("John",12);
         Person p2 = new Person();
-        p1.age = 15;
-        p1.name = "Mahesh";
-        System.out.println(p1.age);
-        System.out.println(p2.name);//Output Null because it not intiziled
+        Person p3 = new Person("Harsh", 25, 928282);
+        // p1.age = 15;
+        // p1.name = "Mahesh";
+        // System.out.println(p1.age);
+        // System.out.println(p2.name);//Output Null because it not intiziled
+        // System.out.println(p1.name);
         System.out.println(p1.name);
-    }
+        System.out.println(p3.name);
+        System.out.println(p3.age);
+        System.out.println(p3.mob);
+    }   
 }
