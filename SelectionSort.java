37f97477870261b53b734b3677a14afb9c6c94bb
AYUSH <mehtaayush6086@gmail.com>
2024-08-18 00:17:22 +0530

    JAVA

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,22 @@
 public class SelectionSort{
+    public static void selctionSort(int num[]){
+        for(int i=0;i<num.length-1;i++){
+            int minPos = i;
+            for(int j=i+1;j<num.length;j++){
+                if(num[minPos]>num[j]){ 
+                 minPos = j;
+                }
+            }
+            int temp = num[minPos];
+            num[minPos] = num[i];
+            num[i] = temp;
+        }
+        for(int k=0;k<num.length;k++){
+            System.out.print(num[k]+" ");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int [] num = {5,3,8,7,9};
+        selctionSort(num);
     }
 }
