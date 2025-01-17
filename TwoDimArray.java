83e0a83e5a9147dd631ab99468cc36d0d05435ae
AYUSH <mehtaayush6086@gmail.com>
2025-01-17 15:02:02 +0530

    JAVA

--- TwoDimArray.java
+++ TwoDimArray.java
@@ -23,5 +23,20 @@ public class TwoDimArray{
             }
         }
         System.out.println("Sum of all the elements: "+sum);
+        //print like a wave
+        for(int j=0;j<n;j++){
+            if(j%2==0){
+            for(int i=0;i<m;i++){
+                System.out.print(marks[i][j]);
+            
+            }
+            }
+            else{
+                for(int i=m-1;i>=0;i--){
+                    System.out.print(marks[i][j]);
+                }
+            }
+        }
+        }
+        
     }
-}
