eb6da39a1ba70ebfd4f687339a8cd4e3c35e06aa
AYUSH <mehtaayush6086@gmail.com>
2025-01-25 22:26:52 +0530

    JAVA

--- Recursion2.java
+++ Recursion2.java
@@ -6,12 +6,24 @@ public class Recursion2{
         }
         printIndex(arr,key,i+1);
     }
+    public static int printSum(int n){
+        if(n==0) return 0;
+        return n+printSum(n-1);
+    }
+    public static int printFibo(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return printFibo(n-1)+printFibo(n-2);
+    }
     public static void main(String[] args) {
         int []arr ={1,2,4,6,1,6,1,3,1};
         int key=1;
         int i=0;
         String str= "abcd";
+        int n=6;
         printIndex(arr,key,i);
-        //System.out.println();
+        System.out.println();
+        System.out.println(printSum(n));
+        System.out.println(printFibo(n));
     }
 }
